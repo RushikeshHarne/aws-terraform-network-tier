1c5ce7b237e31adbd5915fc6ef85f1be72681365
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2026-09-05 10:59:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-# 1. Dynamically discover the central bucket name from SSM Parameter Store (Repo 1).
-data "aws_ssm_parameter" "central_state_bucket" {
+# 1. Dynamically discover the central bucket name from SSM Parameter Store (Repo 1)
+data "aws_ssm_parameter" "central_state_bucket" 
   name = "/terraform/remote_state_bucket"
 }
 
